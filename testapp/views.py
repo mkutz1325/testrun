73e3675037a6dca91990d0625bd3629e4f390665
mkutz1325 <mkutz1325@comcast.net>
2011-11-07 09:24:59 -0800

    fifth commit

--- testapp/views.py
+++ testapp/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.template import Context
+from django.template import Context,loader,RequestContext
 from django.template.loader import get_template
 
 
 def home_page(request):
     template = get_template('home_page.html')
-    output = template
+    c = RequestContext(request, {
+        #can add variables
+        })
+    output = template(template.render(c))
         
     return HttpResponse(output)
